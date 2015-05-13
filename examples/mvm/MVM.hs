e23e39ede6b0b16c394be25b9ec298849dda85a8
Rob Everest <rob.everest@gmail.com>
2015-05-13 17:49:56 +1000

    Added dotp benchmarks to accelerate-mvm

--- examples/mvm/MVM.hs
+++ examples/mvm/MVM.hs
@@ -14,5 +14,15 @@ mvm mat vec
 mvmSeq :: (Elt a, IsNum a) => Acc (Matrix a) -> Acc (Vector a) -> Acc (Vector a)
 mvmSeq mat vec
   = let rows     = toSeqOuter2 mat
-        dotp a b = A.fold (+) 0 (A.zipWith (*) a b)
     in A.collect (A.fromSeqElems (mapSeq (dotp vec) rows))
+
+dotp :: (Elt a, IsNum a) => Acc (Vector a) -> Acc (Vector a) -> Acc (Scalar a)
+dotp a b = A.fold (+) 0 (A.zipWith (*) a b)
+
+dotpSeq :: (Elt a, IsNum a) => Acc (Vector a) -> Acc (Vector a) -> Acc (Scalar a)
+dotpSeq xs ys = A.collect $ A.foldSeq (+) 0 $ A.zipWithSeq (tlift2 (*)) (toSeqElems xs) (toSeqElems ys)
+  where
+    tlift2 :: (Elt a, Elt b, Elt c) => (Exp a -> Exp b -> Exp c) -> (Acc (Scalar a) -> Acc (Scalar b) -> Acc (Scalar c))
+    tlift2 f a b = unit (f (the a) (the b))
+
+    toSeqElems = toSeq (Z:.Split)
--- examples/mvm/Main.hs
+++ examples/mvm/Main.hs
@@ -6,6 +6,7 @@ import Config
 import Prelude                                          as P
 import Data.Label                                       ( get )
 import Data.Array.Accelerate                            as A
+import qualified Data.Array.Accelerate.CUDA                       as CUDA
 import Data.Array.Accelerate.Examples.Internal          as A
 
 
@@ -20,16 +21,20 @@ main = do
   let rows = get configRows conf
       cols = get configCols conf
 
-  let mat = randomMatrix A.uniform rows cols :: Matrix Float
-      vec = randomVector A.uniform cols      :: Vector Float
+  let mat     = randomMatrix A.uniform rows cols   :: Matrix Float
+      vec     = randomVector A.uniform cols        :: Vector Float
+      bigvec1 = randomVector A.uniform (cols*rows) :: Vector Float
+      bigvec2 = randomVector A.uniform (cols*rows) :: Vector Float
 
   let backend   = get optBackend opts
 
+  let bench' name f = runBenchmarks opts rest [bench name f] >> CUDA.performGC
+
   -- Benchmark
   --
-  runBenchmarks opts rest
-    [ bench "mvm"              $ whnf (run2 backend mvm mat) vec
-    , bench "mvmSeq-uselazy"   $ whnf (run1 backend (mvmSeq (use mat))) vec
-    , bench "mvmSeq-nouselazy" $ whnf (run2 backend mvmSeq mat) vec
-    ]
+  -- bench' "mvm"              $ whnf (run2 backend mvm mat) vec
+  -- bench' "mvmSeq-uselazy"   $ whnf (run1 backend (mvmSeq (use mat))) vec
+  -- bench' "mvmSeq-nouselazy" $ whnf (run2 backend mvmSeq mat) vec
+  -- bench' "dotp"             $ whnf (run2 backend dotp bigvec1) bigvec2
+  bench' "dotpSeq"          $ whnf (\bv2 -> run backend (dotpSeq (use bigvec1) (use bv2))) bigvec2
 
