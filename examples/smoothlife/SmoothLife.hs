527a09b6c0ab03f98aa9050cb946afdc9061dbab
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2018-04-03 12:38:41 +1000

    smoothlife: fix

--- examples/smoothlife/SmoothLife.hs
+++ examples/smoothlife/SmoothLife.hs
@@ -34,16 +34,16 @@ smoothlife
     -> Options
     -> Acc (Matrix R)
     -> Acc (Matrix R)
-smoothlife conf opts aa
+smoothlife conf _opts aa
   = aa''
   where
     -- A simulation step
     --
     aaf         = fft2D Forward (complex aa)
-    nf          = zipWith (*) aaf (use krf')
-    mf          = zipWith (*) aaf (use kdf')
-    n           = map (\x -> real x / kflr'') (fft2D Inverse nf)
-    m           = map (\x -> real x / kfld'') (fft2D Inverse mf)
+    nf          = zipWith (*) aaf krf
+    mf          = zipWith (*) aaf kdf
+    n           = map (\x -> real x / the kflr) (fft2D Inverse nf)
+    m           = map (\x -> real x / the kfld) (fft2D Inverse mf)
     aa'         = snm conf sn sm b1 b2 d1 d2 n m
     aa''        = clamp $ zipWith timestepMode aa' aa
 
@@ -71,11 +71,6 @@ smoothlife conf opts aa
     kd          = generate (constant sh) (\ix -> 1 - linear (radius ix) ri b)
     kr          = generate (constant sh) (\ix -> let r = radius ix
                                                  in  linear r ri b * (1 - linear r ra b))
-
-    kflr''      = constant (kflr' `A.indexArray` Z)
-    kfld''      = constant (kfld' `A.indexArray` Z)
-    (kflr', kfld', krf', kdf')
-                = run (get optBackend opts) $ lift (kflr, kfld, krf, kdf)
 
     -- Auxiliary
     --
