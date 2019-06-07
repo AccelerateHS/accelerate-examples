12a413a6b311b71570f0ac347e74c69c04649114
Trevor L. McDonell <trevor.mcdonell@gmail.com>
2019-06-07 18:24:55 +0200

    cleanup julia set implementation

--- examples/julia/Config.hs
+++ examples/julia/Config.hs
@@ -2,8 +2,8 @@
 
 module Config where
 
-import           Data.Label
-import           System.Console.GetOpt
+import Data.Label
+import System.Console.GetOpt
 
 
 data Config = Config
@@ -17,38 +17,38 @@ $(mkLabels [''Config])
 
 defaults :: Config
 defaults = Config
-  { _configWidth        = 800
-  , _configHeight       = 800
+  { _configWidth    = 800
+  , _configHeight   = 600
   }
 
 options :: [OptDescr (Config -> Config)]
 options =
   [ Option []   ["width"]       (ReqArg (set configWidth  . read) "INT")         "visualisation width  (800)"
-  , Option []   ["height"]      (ReqArg (set configHeight . read) "INT")         "visualisation height (800)"
+  , Option []   ["height"]      (ReqArg (set configHeight . read) "INT")         "visualisation height (600)"
   ]
 
 header :: [String]
 header =
-  [ "accelerate-juliasets (c) [2011..2016] Roger Bosman"
+  [ "accelerate-julia (c) [2019] The Accelerate Team"
   , ""
-  , "Usage: accelerate-juliasets [OPTIONS]"
-  , "Or, if using stack: stack exec accelerate-juliasets [OPTIONS]"
+  , "Usage: accelerate-julia [OPTIONS]"
   , ""
   ]
 
 footer :: [String]
 footer =
-  [ "Controls:"
-  , "Key          Action"
-  , "ESC          Exit simulation"
-  , "W            Zoom in"
-  , "S            Zoom out"
-  , "A            Speed up simulation by 25%"
-  , "D            Slow down simulation by 25%"
-  , "Space        Pause/unpause simulation"
-  , "Arrow up     Pan view up"
-  , "Arrow down   Pan view down"
-  , "Arrow left   Pan view left"
-  , "Arrow right  Pan view right"
+  [ ""
+  , "Runtime usage:"
+  , "     ESC           quit"
+  , "     mouse drag    translate display"
+  , "     w/s           zoom in/out"
+  , "     a/d           iteration count"
+  , "     z/c           escape radius"
+  , "     q/e           speed up/slow down"
+  , "     left/right    forward/backwards in time"
+  , "     r             reset"
+  , "     .             print current configuration"
+  , "     space         pause/resume"
   , ""
   ]
+
